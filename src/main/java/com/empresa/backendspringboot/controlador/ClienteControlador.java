267e1e0f2b3c96ad0d7e186b0a59f37292f400da
Boris Vargas <borisvargaspaucara@gmail.com>
2023-01-27 04:37:05 -0400

    cambiando crossOrigin en clienteController

--- src/main/java/com/empresa/backendspringboot/controlador/ClienteControlador.java
+++ src/main/java/com/empresa/backendspringboot/controlador/ClienteControlador.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://soft-custard-a1a7cb.netlify.app"})
 public class ClienteControlador {
 
     @Autowired
